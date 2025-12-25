b92a17837b29db7bf9dc510f0dbc8aedd1407206
Maylon Lima <maylonlima923@gmail.com>
2025-12-25 19:53:50 -0300

    Melhorias no código de herança

--- 1_poo/poo_basica/heranca.py
+++ 1_poo/poo_basica/heranca.py
@@ -1,5 +1,6 @@
 """
-Herança permite criar uma nova classe a partir de uma classe existente, reutilizando e especializando seus atributos e métodos.
+Herança permite criar uma nova classe a partir de uma classe existente,
+reutilizando e especializandoseus atributos e métodos.
 A relação expressa é sempre do tipo “é um” (is-a).
 
 Classe raiz: não possui superclasse.
@@ -19,7 +20,8 @@ Classe abstrata: não deve ser instanciada; serve como modelo conceitual.
 Método abstrato: declarado na classe base e implementado nas subclasses.
 
 Comportamento
-Sobrescrita (override): subclasse redefine um método herdado, permitindo comportamentos diferentes para a mesma interface.
+Sobrescrita (override): subclasse redefine um método herdado, permitindo comportamentos diferentes 
+para a mesma interface.
 
 Python trata herança de forma flexível, sem impor restrições rígidas.
 Classes abstratas, métodos finais e limites de herança são convenções, não imposições da linguagem.
@@ -48,36 +50,33 @@ class Animal(SerVivo):
     def __init__(self, especie, nome):
         super().__init__(especie)
         self.nome = nome
-    def falar(self):
+    def falar(self, msg=None):
         """
         Som emitido pelo ser vivo
         """
-        print('Som Genérico')
+        if msg is None:
+            print('Som Genérico')
+        else:
+            print(f'{self.nome} diz: {msg}')
 
 class Humano():
     """
     Classe vazia apenas para demonstrar herança multipla
     Herança de Implementação
     """
-    pass
 
 class Pessoa(Animal, Humano):
     """
     Classe com herança multipla
     Classe Folha
     """
-    def falar(self, msg):
-        """
-        Aqui a pessoa fala, sendo aplicado override
-        """
-        print(msg)
 
 class Cachorro(Animal):
     """
     Classe com herança simples
     Classe Folha
     """
-    def falar(self):
+    def falar(self, msg=None):
         """
         Aqui o cachorro late, sendo aplicado override
         """
